1a8e0a79f85b2dea298d7c19a0cb03f59466f84e
Sahand N. Mohammed <snqiu210234@uniq.edu.iq>
2024-07-04 11:28:20 +0300

    Enhancement for login and register

---
+++ src/main/java/com/uniq/smartpharma/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.uniq.smartpharma.repository;
+
+import com.uniq.smartpharma.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
